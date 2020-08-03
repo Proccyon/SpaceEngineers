4f385f5433e1a9c018d1caf7230872dbaccf799e
Proccyon <36238733+Proccyon@users.noreply.github.com>
2020-08-03 14:44:37 +0200

    Updated welder arm script
    Welder arm can now move between a given list of coordinates.

--- WeldingArmScript.cs
+++ WeldingArmScript.cs
@@ -1,6 +1,6 @@
 public Program()
 {
-
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
 }
 
 public void Save()
@@ -32,109 +32,130 @@ public class WelderArm
     public IMyMotorAdvancedStator Hinge2;
     public IMyMotorAdvancedStator Hinge3;
 
-    public float X0;
-    public float Y0;
-    public float Z0;
-
-    public float X1;
-    public float Y1;
-    public float Z1;
-
     public Vector3D X_Vector;
     public Vector3D Y_Vector;
     public Vector3D Z_Vector;
 
-    public float Ym;
-    public float Zm;
+    public float WelderMoveSpeed = 2.5f;
 
-    public float NewHinge1Angle; 
-    public float NewHinge2Angle;
+    public Program MyProgram;
 
-    public float NewPistonY1Length;
-    public float NewPistonY2Length; 
-
-
-    public WelderArm(IMyBatteryBlock BottomLeftBattery,IMyBatteryBlock BottomRightBattery,IMyBatteryBlock TopLeftBattery,IMyPistonBase PistonX1,IMyPistonBase PistonX2,IMyPistonBase PistonY1,IMyPistonBase PistonY2, IMyMotorAdvancedStator Hinge1, IMyMotorAdvancedStator Hinge2, IMyMotorAdvancedStator Hinge3)
+    public WelderArm(Program MyProgram, IMyBatteryBlock BottomLeftBattery,IMyBatteryBlock BottomRightBattery,IMyBatteryBlock TopLeftBattery,IMyPistonBase PistonX1,IMyPistonBase PistonX2,IMyPistonBase PistonY1,IMyPistonBase PistonY2, IMyMotorAdvancedStator Hinge1, IMyMotorAdvancedStator Hinge2, IMyMotorAdvancedStator Hinge3)
     {
 
 
-    //-----GetVectorFrame-----//
+        this.MyProgram = MyProgram;
 
-    var X_Vector = (BottomLeftBattery.GetPosition() - BottomRightBattery.GetPosition());
-    X_Vector /= X_Vector.Length();
+        //-----GetVectorFrame-----//
 
-    var Y_Vector = (TopLeftBattery.GetPosition() - BottomLeftBattery.GetPosition());
-    Y_Vector /= Y_Vector.Length();
+        this.X_Vector = (BottomLeftBattery.GetPosition() - BottomRightBattery.GetPosition());
+        this.X_Vector /= this.X_Vector.Length();
 
-    var Z_Vector = -Vector3D.Cross(X_Vector,Y_Vector);
+        this.Y_Vector = (TopLeftBattery.GetPosition() - BottomLeftBattery.GetPosition());
+        this.Y_Vector /= this.Y_Vector.Length();
 
-    this.X1 = 20f;
-    this.Y1 = 20f;
-    this.Z1 = 20f;
+        this.Z_Vector = -Vector3D.Cross(this.X_Vector,this.Y_Vector);
 
-    var MiddleHingeVector = this.GetMidleHingePosition();
-    this.Ym = MiddleHingeVector[0];
-    this.Zm = MiddleHingeVector[1];
-
-    this.NewHinge1Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (this.Zm) / (this.Ym)) );
-    this.NewHinge2Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (this.Z1 - this.Zm) / (this.Y1 - this.Ym)));
-
-    this.NewPistonY1Length = (float)Math.Sqrt( (float)Math.Pow(this.Ym,2) + (float)Math.Pow(this.Zm,2) ) - 7.5f;
-    this.NewPistonY2Length = (float)Math.Sqrt( (float)Math.Pow(this.Y1-Ym,2) + (float)Math.Pow(this.Z1-this.Zm,2)) - 7.5f;
-
-    float PistonSpeed = 0.1f;
-    float HingeSpeed = 0.4f;
-
-
-
-    if(NewPistonY1Length >= 0 && NewPistonY2Length >= 0)
-    {
-        this.SetPiston(PistonY1,NewPistonY1Length,PistonSpeed);
-        this.SetPiston(PistonY2,NewPistonY2Length,PistonSpeed);
-
-        this.SetHinge(Hinge1,NewHinge1Angle,HingeSpeed);
-        this.SetHinge(Hinge2,NewHinge2Angle,HingeSpeed);
-    }
+        this.BottomLeftBattery = BottomLeftBattery;
+        this.BottomRightBattery = BottomRightBattery;
+        this.TopLeftBattery = TopLeftBattery;
+        this.PistonX1 = PistonX1;
+        this.PistonX2 = PistonX2;
+        this.PistonY1 = PistonY1;
+        this.PistonY2 = PistonY2;
+        this.Hinge1 = Hinge1;
+        this.Hinge2 = Hinge2;
+        this.Hinge3 = Hinge3;
 
     }
 
 
-    public float GoTo(float x1, float y1)
+    //Moves Tip of welder to given location
+    public void GoTo(float Y1, float Z1)
     {
-        var MiddleHingeVector = this.GetMidleHingePosition();
-        this.Ym = MiddleHingeVector[0];
-        this.Zm = MiddleHingeVector[1];
+        //Gets new position of middle hinge
+        var MiddleHingeVector = this.GetMiddleHingePosition(Y1,Z1);
+        float Ym = MiddleHingeVector[0];
+        float Zm = MiddleHingeVector[1];
 
-        this.NewHinge1Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (this.Zm) / (this.Ym)) );
-        this.NewHinge2Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (this.Z1 - this.Zm) / (this.Y1 - this.Ym)));
+        //Calculates new hinge angles and piston lengths
+        float NewHinge1Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (Zm) / (Ym)) );
+        float NewHinge2Angle = -NewHinge1Angle+(float)Math.Atan( (Z1 - Zm) / (Y1 - Ym))-0.5f*(float)Math.PI;
 
-        this.NewPistonY1Length = (float)Math.Sqrt( (float)Math.Pow(this.Ym,2) + (float)Math.Pow(this.Zm,2) ) - 7.5f;
-        this.NewPistonY2Length = (float)Math.Sqrt( (float)Math.Pow(this.Y1-Ym,2) + (float)Math.Pow(this.Z1-this.Zm,2)) - 7.5f;
+        float NewPistonY1Length = (float)Math.Sqrt( (float)Math.Pow(Ym,2) + (float)Math.Pow(Zm,2) ) - 7.5f;
+        float NewPistonY2Length = (float)Math.Sqrt( (float)Math.Pow(Y1-Ym,2) + (float)Math.Pow(Z1-Zm,2)) - 7.5f;
 
+        //Distance from location
+        float DestinationDistance = this.GetDistanceFromDestination(Y1,Z1); 
+
+        //this.MyProgram.Echo("Ym = " + Ym.ToString());
+        //this.MyProgram.Echo("Zm = " + Zm.ToString());
+        this.MyProgram.Echo("NewHinge1Angle = " + (NewHinge1Angle*180/(float)Math.PI).ToString());
+        this.MyProgram.Echo("NewHinge2Angle = " + (NewHinge2Angle*180/(float)Math.PI).ToString());
+        this.MyProgram.Echo("NewPistonY1Length = " + NewPistonY1Length.ToString());
+        this.MyProgram.Echo("NewPistonY2Length = " + NewPistonY2Length.ToString());
+        this.MyProgram.Echo("Y1 = " + Y1.ToString());
+        this.MyProgram.Echo("Z1 = " + Z1.ToString());
+        this.MyProgram.Echo("RealY1 = " + Vector3D.Dot(Hinge3.GetPosition() - Hinge1.GetPosition(), this.Y_Vector).ToString());
+        this.MyProgram.Echo("RealZ1 = " + Vector3D.Dot(Hinge3.GetPosition() - Hinge1.GetPosition(), this.Z_Vector).ToString());
+        this.MyProgram.Echo("TickLength = " + (DestinationDistance/this.WelderMoveSpeed).ToString());
+
+        //Does nothing if destination is too close to arm base
         if(NewPistonY1Length >= 0 && NewPistonY2Length >= 0)
         {
-    
-            this.SetPiston(PistonY1,NewPistonY1Length,PistonSpeed);
-            this.SetPiston(PistonY2,NewPistonY2Length,PistonSpeed);
+            
+            float JourneyTime = (DestinationDistance / this.WelderMoveSpeed);
+            if(JourneyTime < 0.25f)
+            {
+                JourneyTime = 0.25f;
+            }
 
-            this.SetHinge(Hinge1,NewHinge1Angle,HingeSpeed);
-            this.SetHinge(Hinge2,NewHinge2Angle,HingeSpeed);
+            //Calculates speed so that all parts take the same amount of time to move
+            float Hinge1Speed = (NewHinge1Angle - this.Hinge1.Angle) / JourneyTime;
+            float Hinge2Speed = (NewHinge2Angle - this.Hinge2.Angle) / JourneyTime;
+            float PistonY1Speed = (NewPistonY1Length - this.GetPistonLength(this.PistonY1)) / JourneyTime;
+            float PistonY2Speed = (NewPistonY2Length - this.GetPistonLength(this.PistonY2)) / JourneyTime;
+
+            //Sets speed as to move to new position
+            this.SetHinge(this.Hinge1,NewHinge1Angle,Hinge1Speed);
+            this.SetHinge(this.Hinge2,NewHinge2Angle,Hinge2Speed);
+
+            this.SetPiston(this.PistonY1,NewPistonY1Length,PistonY1Speed);
+            this.SetPiston(this.PistonY2,NewPistonY2Length,PistonY2Speed);
+
         }
-    }
 
     }
 
-
-    public float ParabolicFormula(float y)
-    {
-        return 2 * y*this.Z1 / this.Y1 - (float)Math.Pow(y,2) * this.Z1 / (float)Math.Pow(this.Y1,2);
-    }
-
-    public List<float> GetMidleHingePosition()
+    //Gets the distance from tip of welder to destination
+    public float GetDistanceFromDestination(float Y1, float Z1)
     {
 
-        float Ym = ( -(this.Y1 + 2f * (float)Math.Pow(this.Z1,2)/this.Y1) + (float)Math.Sqrt( (float)Math.Pow(this.Y1,2) + 2f * (float)Math.Pow(this.Z1,4) / (float)Math.Pow(this.Y1,2) + 2f * (float)Math.Pow(this.Z1,2))) / (-2f * (float)Math.Pow(this.Z1,2) / (float)Math.Pow(this.Y1,2));
-        float Zm = this.ParabolicFormula(Ym);
+        float PistonY1Length = this.GetPistonLength(this.PistonY1);
+        float PistonY2Length = this.GetPistonLength(this.PistonY2);
+        float Hinge1Angle = this.Hinge1.Angle;
+        float Hinge2Angle = this.Hinge2.Angle;
+
+        float Y = (PistonY1Length+7.5f)*(float)Math.Sin(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Cos((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI));
+        float Z = (PistonY1Length+7.5f)*(float)Math.Cos(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Sin((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI));
+
+        //this.MyProgram.Echo(Y.ToString());
+        //this.MyProgram.Echo(Z.ToString());
+        return (float)Math.Sqrt((float)Math.Pow(Y1-Y,2) + (float)Math.Pow(Z1-Z,2));
+    }
+
+    //Welder arm follows a parabola to decrease free parameters
+    public float ParabolicFormula(float Y, float Y1, float Z1)
+    {
+        return 2 * Y * Z1 / Y1 - (float)Math.Pow(Y,2) * Z1 / (float)Math.Pow(Y1,2);
+    }
+
+    //Gets position of middle hinge
+    public List<float> GetMiddleHingePosition(float Y1, float Z1)
+    {
+
+        float Ym = ( -(Y1 + 2f * (float)Math.Pow(Z1,2) / Y1) + (float)Math.Sqrt( (float)Math.Pow(Y1,2) + 2f * (float)Math.Pow(Z1,4) / (float)Math.Pow(Y1,2) + 2f * (float)Math.Pow(Z1,2))) / (-2f * (float)Math.Pow(Z1,2) / (float)Math.Pow(Y1,2));
+        float Zm = this.ParabolicFormula(Ym,Y1,Z1);
 
         List<float> MiddleHingeVector = new List<float>();
         MiddleHingeVector.Add(Ym);
@@ -143,24 +164,24 @@ public class WelderArm
         return MiddleHingeVector;
     }
 
-
     public void SetHinge(IMyMotorAdvancedStator Hinge, float Angle, float Speed)
     {
 
-        Hinge.UpperLimitRad = Angle;
-        Hinge.LowerLimitRad = Angle;
-
-        if(Hinge.Angle >= Angle)
-        {
-            Hinge.TargetVelocityRPM = -Speed;
-        }
-        else
-        {
-            Hinge.TargetVelocityRPM = Speed;
-        }
+        //Hinge.UpperLimitRad = Angle;
+        //Hinge.LowerLimitRad = Angle;
+        Hinge.TargetVelocityRad = Speed;
     }
 
+    public void SetPiston(IMyPistonBase Piston, float Distance, float Speed)
+    {
 
+        //Piston.MinLimit = Distance;
+        //Piston.MaxLimit = Distance;
+        Piston.Velocity = Speed;
+    
+    }
+
+    //Gets current length of a piston
     public float GetPistonLength(IMyPistonBase Piston)
     {
         string PistonInfo = Piston.DetailedInfo;
@@ -169,25 +190,62 @@ public class WelderArm
         return float.Parse(PistonInfoList[0]);
     }
 
-    public void SetPiston(IMyPistonBase Piston, float Distance, float Speed)
+}
+
+public Vector3D GetCoordFromPath(float s, List<Vector3D> CoordList)
+{
+
+    float S_Path = 0f;
+    Vector3D LastCoord = CoordList[0];
+    float SegmentLength = 0f;
+
+    foreach(Vector3D Coord in CoordList)
     {
+        //Length betweeb Coord and LastCoord
+        SegmentLength = (float)(Coord-LastCoord).Length();
 
-        Piston.MinLimit = Distance;
-        Piston.MaxLimit = Distance;
-
-        if(GetPistonLength(Piston) >= Distance)
+        //Total lentgh of path between CoordList[0] and Coord
+        S_Path += SegmentLength;
+        
+        if(s < S_Path)
         {
-            Piston.Velocity = -Speed;
-        }
-        else
-        {
-            Piston.Velocity = Speed;
+
+            float TraversedSegmentLength = SegmentLength + s - S_Path;
+            return TraversedSegmentLength * (Coord - LastCoord) / SegmentLength + LastCoord;
+
         }
 
+        LastCoord = Coord;
     }
+
+    return LastCoord;
 
 }
 
+public void FollowPath(WelderArm WeldArm, List<Vector3D> CoordList, float CloseDistance = 0.5f)
+{
+
+    Vector3D OldCoord = GetCoordFromPath(s,CoordList);
+    float OldDistance = WeldArm.GetDistanceFromDestination((float)OldCoord.Y,(float)OldCoord.Z);
+
+    //Echo(OldDistance.ToString());
+
+    if(OldDistance <= CloseDistance)
+    {
+        s += WeldArm.WelderMoveSpeed * 0.1f;
+    }
+
+    Vector3D NewCoord = GetCoordFromPath(s,CoordList);
+    WeldArm.GoTo((float)NewCoord.Y,(float)NewCoord.Z);
+
+    //float NewDistance = WeldArm.GetDistanceFromDestination(NewCoord[1],NewCoord[2]);
+
+
+}
+
+
+
+public float s = 0f;
 
 public void Main(string argument, UpdateType updateSource)
 {
@@ -209,14 +267,18 @@ public void Main(string argument, UpdateType updateSource)
 
     //-----GetVectorFrame-----//
 
-    WelderArm MainWelder = new WelderArm(BottomLeftBattery,BottomRightBattery,TopLeftBattery,PistonX1,PistonX2,PistonY1,PistonY2,Hinge1,Hinge2,Hinge3);
+    WelderArm MainWelder = new WelderArm(this,BottomLeftBattery,BottomRightBattery,TopLeftBattery,PistonX1,PistonX2,PistonY1,PistonY2,Hinge1,Hinge2,Hinge3);
 
-    Echo("NewHinge1Angle = " + (MainWelder.NewHinge1Angle*180/(float)Math.PI).ToString());
-    Echo("NewHinge2Angle = " + (MainWelder.NewHinge2Angle*180/(float)Math.PI).ToString());
-    Echo("NewPistonY1Length = " + MainWelder.NewPistonY1Length.ToString());
-    Echo("NewPistonY2Length = " + MainWelder.NewPistonY2Length.ToString());
-    Echo("Ym = " + MainWelder.Ym.ToString());
-    Echo("Zm = " + MainWelder.Zm.ToString());
+    List<Vector3D> WelderPath = new List<Vector3D>();
+    WelderPath.Add(new Vector3D(15f,15f,15f));
+    WelderPath.Add(new Vector3D(15f,30f,15f));
+    WelderPath.Add(new Vector3D(15f,30f,10f));
+    WelderPath.Add(new Vector3D(15f,15f,10f));
+    WelderPath.Add(new Vector3D(15f,15f,15f));
+
+    FollowPath(MainWelder,WelderPath);
+
+    Echo("s = " + s.ToString());
 
 
 }
