52d2a2662e710482e6abc1b8b76901b69a84eddc
Proccyon <36238733+Proccyon@users.noreply.github.com>
2020-08-21 13:39:44 +0200

    Improved welder precision
    Can now move the exact position of the welder tip. Can also now adjust the move speed along the path.

--- WeldingArmScript.cs
+++ WeldingArmScript.cs
@@ -29,8 +29,6 @@ public class WelderArm
     public IMyMotorAdvancedStator Hinge2;
     public IMyMotorAdvancedStator Hinge3;
 
-    public float WelderMoveSpeed = 2.5f;
-
     public Program MyProgram;
 
     public WelderArm(Program MyProgram,IMyPistonBase PistonX1,IMyPistonBase PistonX2,IMyPistonBase PistonY1,IMyPistonBase PistonY2, IMyMotorAdvancedStator Hinge1, IMyMotorAdvancedStator Hinge2, IMyMotorAdvancedStator Hinge3)
@@ -48,49 +46,92 @@ public class WelderArm
 
     }
 
-
-    //Moves Tip of welder to given location
-    public void GoTo(float X1, float Y1, float Z1, Func<float,float,List<float>> MiddleHingeMethod)
+    public List<float> GetNewSystemParameters(float X1, float Y1, float Z1, Func<float,float,List<float>> MiddleHingeMethod)
     {
-        //Gets new position of middle hinge
+        //Calculates position of middle hinge
         var MiddleHingeVector = MiddleHingeMethod(Y1,Z1);
         float Ym = MiddleHingeVector[0];
         float Zm = MiddleHingeVector[1];
 
         //Calculates new hinge angles and piston lengths
-        float NewHinge1Angle = -(0.5f*(float)Math.PI  - (float)Math.Atan( (Zm) / (Ym)) );
+        float NewHinge1Angle = -(float)Math.Atan( (Ym) / (Zm));
         float NewHinge2Angle = -NewHinge1Angle+(float)Math.Atan( (Z1 - Zm) / (Y1 - Ym))-0.5f*(float)Math.PI;
         float NewHinge3Angle = 0.5f*(float)Math.PI + NewHinge1Angle + NewHinge2Angle;
 
         float NewPistonY1Length = (float)Math.Sqrt( (float)Math.Pow(Ym,2) + (float)Math.Pow(Zm,2) ) - 7.5f;
         float NewPistonY2Length = (float)Math.Sqrt( (float)Math.Pow(Y1-Ym,2) + (float)Math.Pow(Z1-Zm,2)) - 7.5f;
 
+        List<float> SystemParameterList = new List<float>();
+        SystemParameterList.Add(NewHinge1Angle);
+        SystemParameterList.Add(NewHinge2Angle);
+        SystemParameterList.Add(NewHinge3Angle);
+        SystemParameterList.Add(NewPistonY1Length);
+        SystemParameterList.Add(NewPistonY2Length);
+
+        return SystemParameterList;
+
+
+    }
+
+
+
+    //Moves Tip of welder to given location
+    public void GoTo(float X1, float Y1, float Z1, float MoveSpeed, Func<float,float,List<float>> MiddleHingeMethod)
+    {
+
+        int Iterations = 5;
+        float X1_Adjusted = X1;
+        float Y1_Adjusted = Y1;
+        float Z1_Adjusted = Z1 + 5f;
+
+        List<float> SystemParameterList;
+        float NewHinge1Angle = 0; 
+        float NewHinge2Angle = 0;
+        float NewHinge3Angle = 0;
+        float NewPistonY1Length = 0;
+        float NewPistonY2Length = 0;
+
+        for (int i = 0; i < Iterations; i++) 
+        {
+
+            SystemParameterList = this.GetNewSystemParameters(X1_Adjusted,Y1_Adjusted,Z1_Adjusted,MiddleHingeMethod);
+
+            NewHinge1Angle = SystemParameterList[0];
+            NewHinge2Angle = SystemParameterList[1];
+            NewHinge3Angle = SystemParameterList[2];
+            NewPistonY1Length = SystemParameterList[3];
+            NewPistonY2Length = SystemParameterList[4];
+
+            Y1_Adjusted = Y1 - 2.5f * (float)Math.Sin(NewHinge3Angle);
+            Z1_Adjusted = Z1 + 2.5f + 2.5f * (float)Math.Cos(NewHinge3Angle);
+
+        }
+
         //Distance from location
         float DestinationDistance = this.GetDistanceFromDestination(X1,Y1,Z1); 
 
-        this.MyProgram.Echo("Ym = " + Ym.ToString());
-        this.MyProgram.Echo("Zm = " + Zm.ToString());
         this.MyProgram.Echo("NewHinge1Angle = " + (NewHinge1Angle*180/(float)Math.PI).ToString());
         this.MyProgram.Echo("NewHinge2Angle = " + (NewHinge2Angle*180/(float)Math.PI).ToString());
         this.MyProgram.Echo("NewHinge3Angle = " + (NewHinge3Angle*180/(float)Math.PI).ToString());
         this.MyProgram.Echo("NewPistonY1Length = " + NewPistonY1Length.ToString());
         this.MyProgram.Echo("NewPistonY2Length = " + NewPistonY2Length.ToString());
+        this.MyProgram.Echo("DestinationDistance = " + DestinationDistance.ToString());
+        this.MyProgram.Echo("Y1_Adjusted = " + Y1_Adjusted.ToString());
+        this.MyProgram.Echo("Z1_Adjusted = " + Z1_Adjusted.ToString());
         this.MyProgram.Echo("Y1 = " + Y1.ToString());
         this.MyProgram.Echo("Z1 = " + Z1.ToString());
-        //this.MyProgram.Echo("RealY1 = " + Vector3D.Dot(Hinge3.GetPosition() - Hinge1.GetPosition(), this.Y_Vector).ToString());
-        //this.MyProgram.Echo("RealZ1 = " + Vector3D.Dot(Hinge3.GetPosition() - Hinge1.GetPosition(), this.Z_Vector).ToString());
 
         //Does nothing if destination is too close to arm base
         if(NewPistonY1Length >= 0 && NewPistonY2Length >= 0)
         {
             
-            float JourneyTime = (DestinationDistance / this.WelderMoveSpeed);
+            float JourneyTime = (DestinationDistance / MoveSpeed);
             if(JourneyTime < 0.25f)
             {
                 JourneyTime = 0.25f;
             }
 
-            //Calculates speed so that all parts take the same amount of time to move
+            //Calculates speed so that all parts take the same amount of time to movepis
             float Hinge1Speed = (NewHinge1Angle - this.Hinge1.Angle) / JourneyTime;
             float Hinge2Speed = (NewHinge2Angle - this.Hinge2.Angle) / JourneyTime;
             float Hinge3Speed = (NewHinge3Angle - this.Hinge3.Angle) / JourneyTime;
@@ -114,9 +155,6 @@ public class WelderArm
 
     }
 
-
-
-
     //Gets the distance from tip of welder to destination
     public float GetDistanceFromDestination(float X1, float Y1, float Z1)
     {
@@ -127,10 +165,12 @@ public class WelderArm
         float PistonX2Length = this.GetPistonLength(this.PistonX2);
         float Hinge1Angle = this.Hinge1.Angle;
         float Hinge2Angle = this.Hinge2.Angle;
+        float Hinge3Angle = this.Hinge3.Angle;
+
 
         float X = PistonX1Length + PistonX2Length;
-        float Y = (PistonY1Length+7.5f)*(float)Math.Sin(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Cos((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI));
-        float Z = (PistonY1Length+7.5f)*(float)Math.Cos(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Sin((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI));
+        float Y = (PistonY1Length+7.5f)*(float)Math.Sin(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Cos((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI)) + 2.5f*(float)Math.Sin(Hinge3Angle);
+        float Z = (PistonY1Length+7.5f)*(float)Math.Cos(-Hinge1Angle)+(PistonY2Length+7.5f)*(float)Math.Sin((Hinge1Angle+Hinge2Angle+0.5f*(float)Math.PI)) -2.5f - 2.5f*(float)Math.Cos(Hinge3Angle);
 
         return (float)Math.Sqrt((float)Math.Pow(X1-X,2) + (float)Math.Pow(Y1-Y,2) + (float)Math.Pow(Z1-Z,2));
     }
@@ -206,49 +246,65 @@ public class WelderArm
 
 }
 
-public Vector3D GetCoordFromPath(float s, List<Vector3D> CoordList)
+public Vector3D GetCoordFromPath(float s, List<Vector3D> CoordList, List<float> SpeedList, out float NewSpeed)
 {
 
     float S_Path = 0f;
     Vector3D LastCoord = CoordList[0];
     float SegmentLength = 0f;
 
-    foreach(Vector3D Coord in CoordList)
+    //foreach(Vector3D Coord in CoordList)
+    for(int i=0; i<CoordList.Count; i++)
     {
-        //Length betweeb Coord and LastCoord
+
+        Vector3D Coord = CoordList[i];
+
+        //Length between Coord and LastCoord
         SegmentLength = (float)(Coord-LastCoord).Length();
 
         //Total lentgh of path between CoordList[0] and Coord
         S_Path += SegmentLength;
         
-        if(s < S_Path)
+        if(s <= S_Path)
         {
 
-            float TraversedSegmentLength = SegmentLength + s - S_Path;
-            return TraversedSegmentLength * (Coord - LastCoord) / SegmentLength + LastCoord;
+            NewSpeed = SpeedList[i];
+            if(SegmentLength == 0)
+            {
+                return CoordList[0];
+            }
+            else
+            {
+                float TraversedSegmentLength = SegmentLength + s - S_Path;
+                return TraversedSegmentLength * (Coord - LastCoord) / SegmentLength + LastCoord;
+            }
 
         }
 
         LastCoord = Coord;
     }
 
+    NewSpeed = SpeedList[SpeedList.Count-1];
     return LastCoord;
 
 }
 
-public void FollowPath(WelderArm WeldArm, List<Vector3D> CoordList, float CloseDistance = 0.5f)
+
+
+public void FollowPath(WelderArm WeldArm, List<Vector3D> CoordList, List<float> SpeedList, float CloseDistance = 0.5f)
 {
 
-    Vector3D OldCoord = GetCoordFromPath(s,CoordList);
+    float NewSpeed = 0f;
+    Vector3D OldCoord = GetCoordFromPath(s,CoordList,SpeedList,out NewSpeed);
     float OldDistance = WeldArm.GetDistanceFromDestination((float)OldCoord.X, (float)OldCoord.Y, (float)OldCoord.Z);
 
     if(OldDistance <= CloseDistance)
     {
-        s += WeldArm.WelderMoveSpeed * 0.1f;
+        s += NewSpeed*0.1f;
     }
 
-    Vector3D NewCoord = GetCoordFromPath(s,CoordList);
-    WeldArm.GoTo((float)NewCoord.X, (float)NewCoord.Y,(float)NewCoord.Z, WeldArm.GetMiddleHingePositionP1);
+    Vector3D NewCoord = GetCoordFromPath(s,CoordList,SpeedList,out NewSpeed);
+    WeldArm.GoTo((float)NewCoord.X, (float)NewCoord.Y,(float)NewCoord.Z, NewSpeed, WeldArm.GetMiddleHingePositionP1);
 
 }
 
@@ -275,15 +331,20 @@ public void Main(string argument, UpdateType updateSource)
     WelderArm MainWelder = new WelderArm(this,PistonX1,PistonX2,PistonY1,PistonY2,Hinge1,Hinge2,Hinge3);
 
     List<Vector3D> WelderPath = new List<Vector3D>();
-    WelderPath.Add(new Vector3D(0f,15f,15f));
-    WelderPath.Add(new Vector3D(12.5f,15f,15f));
-    WelderPath.Add(new Vector3D(12.5f,30f,15f));
-    WelderPath.Add(new Vector3D(12.25f,24f,5f));
-    WelderPath.Add(new Vector3D(12.5f,30f,15f));
-    WelderPath.Add(new Vector3D(12.5f,15f,15f));
-    WelderPath.Add(new Vector3D(0f,15f,15f));
+    WelderPath.Add(new Vector3D(0f,22.5f,11f));
+    WelderPath.Add(new Vector3D(10.75f,22.5f,11f));
+    WelderPath.Add(new Vector3D(10.75f,22.5f,3.5f));
+    WelderPath.Add(new Vector3D(10.75f,22.5f,11f));
+    WelderPath.Add(new Vector3D(0f,22.5f,11f));
 
-    FollowPath(MainWelder,WelderPath);
+    List<float> WelderSpeedList = new List<float>();
+    WelderSpeedList.Add(2f);
+    WelderSpeedList.Add(2f);
+    WelderSpeedList.Add(1.5f);
+    WelderSpeedList.Add(0.15f);
+    WelderSpeedList.Add(2f);
+
+    FollowPath(MainWelder,WelderPath,WelderSpeedList);
 
     Echo("s = " + s.ToString());
 
